4f23b773b85af0a9728832a8d2a4db7076236eee
Giselle Salgado <gfsalgado1@cps.edu>
2021-03-22 17:56:17 -0500

    Added Hello, changed deployment target

--- RoomiesIntrpo/ViewController.swift
+++ RoomiesIntrpo/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        print("hello")
         textField?.text = answer
        
         // Do any additional setup after loading the view.
