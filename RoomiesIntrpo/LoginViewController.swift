0cfa9bedeeb6954f6c8b26aebf2c3b17c0314646
Kelsi Sumter <Ksumter28@gmail.com>
2021-03-22 15:54:19 -0500

    Basics

--- RoomiesIntrpo/LoginViewController.swift
+++ RoomiesIntrpo/LoginViewController.swift
@@ -15,19 +15,23 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         schoolLabel?.text = ("There are ??% of people at " + school + " that have those same answers ")
+        
         // Do any additional setup after loading the view.
     }
+    
     @IBOutlet weak var schoolLabel: UILabel!
     
-
-    /*
+ 
+    
+      
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
+    
 
-}
