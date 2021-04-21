cf9669a31a6738023e83cc483eb50480b166c38a
Amirah Ibrahim <amirahibrahim22@gmail.com>
2021-04-21 17:05:52 -0500

    Revert "Tinder Swipe"
    This reverts commit a95b5b4eb8e73aa054c578b0fc41a331323c4bf7.

--- RoomiesIntrpo/TinderSwipeViewController.swift
+++ RoomiesIntrpo/TinderSwipeViewController.swift
@@ -6,47 +6,13 @@
 //
 
 import UIKit
-import Shuffle_iOS
 
-class SwipeCard: UIViewController {
+class TinderSwipeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-    
-    func card(fromImage image: UIImage) -> SwipeCard {
-      let card = SwipeCard()
-//      card.swipeDirections = [.left, .right]
-//      card.content = UIImageView(image: image)
-//
-//      let leftOverlay = UIView()
-//      leftoverlay.backgroundColor = .green
-//
-//      let rightOverlay = UIView()
-//      rightOverlay.backgroundColor = .red
-//
-//      card.setOverlays([.left: leftOverlay, .right: rightOverlay])
-      
-      return card
-    }
-    
-   
-    class ViewController: UIViewController {
-      let cardStack = SwipeCardStack()
-      
-      let cardImages = [
-          UIImage(named: "cardImage1"),
-          UIImage(named: "cardImage2"),
-          UIImage(named: "cardImage3")
-      ]
-      
-      override func viewDidLoad() {
-        super.viewDidLoad()
-//        view.addSubview(cardStack)
-//        cardStack.frame = view.safeAreaLayoutGuide.layoutFrame
-      }
     }
     
 
